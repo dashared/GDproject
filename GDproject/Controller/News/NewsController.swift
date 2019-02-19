e6e94ee74951dbedd6696ab518f8b639dfac4432
Darya Rednikina <rednikina.com@yandex.ru>
2019-02-19 15:36:48 +0300

    changed concept of channels. needs great modifications

--- GDproject/Controller/News/NewsController.swift
+++ GDproject/Controller/News/NewsController.swift
@@ -8,11 +8,39 @@
 
 import UIKit
 import TinyConstraints
+import Cartography
 
 // MARK:- Controller with posts and channels availiable.
 // Search is availiable within every table (posts and channels). Has button-functionality for boths post and chnnels
-class NewsController: UIViewController
+class NewsController: UIViewController, UISearchControllerDelegate, UITableViewDelegate, UITableViewDataSource
 {
+    
+    
+    var array = ["Faculty 1", "Faculty 2","Faculty 3","Faculty 4","Faculty 5","Faculty 6","Faculty 7","Faculty 8","Faculty 9","Faculty 10","Faculty 11","Faculty 12","Faculty 13","Faculty 14","Faculty 15","Faculty 16","Faculty 17","Faculty 18"]
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return array.count
+    }
+
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+
+        cell.textLabel?.text = array[indexPath.row]
+        return cell
+    }
+
+    func presentSearchController(_ searchController: UISearchController) {
+        print("here")
+        setUpTableView()
+        displayTableView() //setup
+        // this is 0.3
+        animateTable(some: true)
+    }
+
+    func willDismissSearchController(_ searchController: UISearchController) {
+        dismissTable()
+    }
 
     @IBOutlet weak var tableView: UITableView!
     
@@ -21,24 +49,25 @@ class NewsController: UIViewController
     var dataSourse: [Post] = [Post(dataArray: [.text("Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression. Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500, quand un imprimeur anonyme assembla ensemble des morceaux de texte pour réaliser un livre spécimen de polices de texte. Il n'a pas fait que survivre cinq siècles, mais s'est aussi adapté à la bureautique informatique, sans que son contenu n'en soit modifié. Il a été popularisé dans les années 1960 grâce à la vente de feuilles Letraset contenant des passages du Lorem Ipsum, et, plus récemment, par son inclusion dans des applications de mise en page de texte, comme Aldus PageMaker. \n On sait depuis longtemps que travailler avec du texte lisible et contenant du sens est source de distractions, et empêche de se concentrer sur la mise en page elle-même. L'avantage du Lorem Ipsum sur un texte générique comme 'Du texte. Du texte. Du texte.' est qu'il possède une distribution de lettres plus ou moins normale, et en tout cas comparable avec celle du français standard. De nombreuses suites logicielles de mise en page ou éditeurs de sites Web ont fait du Lorem Ipsum leur faux texte par défaut, et une recherche pour 'Lorem Ipsum' vous conduira vers de nombreux sites qui n'en sont encore qu'à leur phase de construction. Plusieurs versions sont apparues avec le temps, parfois par accident, souvent intentionnellement (histoire d'y rajouter de petits clins d'oeil, voire des phrases embarassantes).")], from: User(name: "vbogomazova", id: 2, fullName: "Богомазова Вероника Львовна"), date: "14.02.19 в 12:05")]
     
     var news = NewsVC()
-    var channels = ChannelsController()
     
     override func viewDidLoad()
     {
         super.viewDidLoad()
-        tableView.register(HeaderNewsChannels.self, forCellReuseIdentifier: headerNewsChannelsVC)
         tableView.register(PostViewCell.self, forCellReuseIdentifier: postCellId)
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        
+        //searchController.delegate = self
+        searchController.obscuresBackgroundDuringPresentation = false
         
         news.dataSourse = dataSourse
         news.viewController = self
         
-        news.type = .NEWS("Posts", "Channels")
         
         tableView.delegate = news
         tableView.dataSource = news
         tableView.reloadData()
         
-        
+        news.type = .NEWS
         
         setUpNavigationItemsforPosts()
         searchController.searchBar.placeholder  = "Search anything"
@@ -49,8 +78,55 @@ class NewsController: UIViewController
         setUpBanner()
     }
     
+    
+    var facultyTableView = UITableView()
+    
+    func setUpTableView()
+    {
+        facultyTableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        facultyTableView.delegate = self
+        facultyTableView.dataSource = self
+        let screenSize: CGRect = UIScreen.main.bounds
+        
+        let screenWidth = screenSize.width
+        let screenHeight = screenSize.height
+        facultyTableView.frame = CGRect(x: 0, y: 0, width: Int(screenWidth), height: Int(screenHeight))
+    }
+    
+    func displayTableView(){
+        
+        // the most important line
+        facultyTableView.translatesAutoresizingMaskIntoConstraints = false
+        facultyTableView.backgroundColor = UIColor(white: 1, alpha: 1)
+        view.addSubview(facultyTableView)
+        
+        facultyTableView.isHidden = true
+        
+        // now
+        constrain(facultyTableView, view){
+            tableView, view in
+            
+            tableView.top == view.safeAreaLayoutGuide.top
+            tableView.left == view.left
+            tableView.right == view.right
+            tableView.bottom == view.safeAreaLayoutGuide.bottom
+        }
+        
+    }
+    
+    // animation of the table cells
+    func animateTable(some: Bool){
+        self.facultyTableView.reloadData()
+        self.facultyTableView.isHidden = false
+    }
+    
+    func dismissTable() {
+        self.facultyTableView.isHidden = true
+    }
+    
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
+        print("hello")
         navigationController?.navigationBar.prefersLargeTitles = true
     }
     
@@ -62,13 +138,7 @@ class NewsController: UIViewController
         tableView.reloadData()
     }
     
-    func setUpNavigationItemsForChannels(){
-        navigationItem.title = "Channels"
-        navigationItem.rightBarButtonItems = [UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addChannel))]
-        tableView.delegate = channels
-        tableView.dataSource = channels
-        tableView.reloadData()
-    }
+    //navigationItem.rightBarButtonItems = [UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addChannel))]
     
     @objc func writePost(_ barItem: UIBarButtonItem)
     {
--- GDproject/Controller/News/NewsVC.swift
+++ GDproject/Controller/News/NewsVC.swift
@@ -16,27 +16,64 @@ class NewsVC: UIViewController, UITableViewDelegate, UITableViewDataSource{
     
     var viewController: UIViewController?
     
+    func numberOfSections(in tableView: UITableView) -> Int {
+        switch type {
+        case .NONE:
+            return 1
+        default:
+            return 2
+        }
+    }
+    
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath)
     {
         tableView.deselectRow(at: indexPath, animated: true)
         
-        let vc = viewController!.storyboard!.instantiateViewController(withIdentifier: fullPostControllerId) as! FullPostController
-        
-        vc.post = dataSourse[indexPath.row]
-        viewController!.navigationController!.pushViewController(vc, animated: true)
+        if indexPath.section == 1 && type == .NEWS || type == .NONE {
+            let vc = viewController!.storyboard!.instantiateViewController(withIdentifier: fullPostControllerId) as! FullPostController
+            
+            vc.post = dataSourse[indexPath.row]
+            viewController!.navigationController!.pushViewController(vc, animated: true)
+        }
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return dataSourse.count
+        switch type {
+        case .NONE:
+            return dataSourse.count
+        default:
+            switch section {
+            case 0:
+                return 1
+            default:
+                return dataSourse.count
+            }
+        }
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
     {
-        let cell = tableView.dequeueReusableCell(withIdentifier: postCellId, for: indexPath) as! PostViewCell
-        
-        cell.fill(with: dataSourse[indexPath.row].dataArray, false, post: dataSourse[indexPath.row])
-        cell.selectionStyle = .none
-        return cell
+        switch type {
+        case .NONE:
+            let cell = tableView.dequeueReusableCell(withIdentifier: postCellId, for: indexPath) as! PostViewCell
+            
+            cell.fill(with: dataSourse[indexPath.row].dataArray, false, post: dataSourse[indexPath.row])
+            cell.selectionStyle = .none
+            return cell
+        default:
+            switch indexPath.section {
+            case 0:
+                let cell = tableView.dequeueReusableCell(withIdentifier: "cell")!
+                cell.textLabel?.text = "List of channels"
+                return cell
+            default:
+                let cell = tableView.dequeueReusableCell(withIdentifier: postCellId, for: indexPath) as! PostViewCell
+                
+                cell.fill(with: dataSourse[indexPath.row].dataArray, false, post: dataSourse[indexPath.row])
+                cell.selectionStyle = .none
+                return cell
+            }
+        }
     }
     
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
@@ -46,41 +83,10 @@ class NewsVC: UIViewController, UITableViewDelegate, UITableViewDataSource{
     func tableView(_ tableView: UITableView, estimatedHeightForRowAt indexPath: IndexPath) -> CGFloat {
         return 100.0
     }
-    
-    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
-        switch type {
-        case .NONE:
-            return 0
-        default:
-            return 46
-        }
-    }
-    
-    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView?
-    {
-        let cell = tableView.dequeueReusableCell(withIdentifier: headerNewsChannelsVC) as! HeaderNewsChannels
-        
-        cell.vc = viewController as? NewsController
-        cell.vcProfile = viewController as? ProfileViewController
-        
-        let view = UIView(frame: CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width, height: 46.0))
-        view.addSubview(cell)
-        cell.edgesToSuperview()
-        
-        switch type {
-        case .NONE:
-            return nil
-        default:
-            return view
-        }
-    }
-    
 }
 
 
 enum HeaderType {
     case NONE
-    case NEWS(String, String)
-    case PROFILE(String, String)
-    case BASIC_INFO(String, String)
+    case NEWS
 }
